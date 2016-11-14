406e505a69341a38c3c3de4a26b71a5f0c655c06
withwooyong <withwooyong@gmail.com>
2016-11-15 08:34:47 +0900

    test

--- test-maven/src/main/java/elasticsearchtest/ElasticsearchTest.java
+++ test-maven/src/main/java/elasticsearchtest/ElasticsearchTest.java
@@ -4,6 +4,7 @@ public class ElasticsearchTest {
 
 	public static void main(String[] args) {
 		
+		
 		//TransportClient client = new PreBuiltTransportClient(Settings.EMPTY).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("119.149.188.226"), 9300));
 		        //.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("host2"), 9300));
 
